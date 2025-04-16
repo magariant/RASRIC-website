8ab8dd64be6494e964ba92252a3f6442c050fa4d
magariant <magarian.t@gmail.com>
2025-04-16 10:01:51 -0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Gallery loading from Google Drive...');
+function showTab(category) {
+  const gallery = document.getElementById('gallery');
+  gallery.innerHTML = `<p>Loading ${category} images...</p>`;
+  // Placeholder: you'd replace this with a fetch or API call to load images from Google Drive manually
+  setTimeout(() => {
+    gallery.innerHTML = `
+      <img src="https://via.placeholder.com/200x300" alt="${category} Image 1" />
+      <img src="https://via.placeholder.com/300x200" alt="${category} Image 2" />
+    `;
+  }, 800);
+}
